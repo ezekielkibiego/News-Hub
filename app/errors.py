7fa91d368ebbb3dce04c9d8cc8ead8bbbd99b8d6
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-10-31 16:21:32 +0300

    added css links and bootstrap

--- app/errors.py
+++ app/errors.py
@@ -8,3 +8,13 @@
 #     Function to render the 404 error page
 #     """
 #     return render_template('404.html'), 404
+
+from flask import render_template
+from app import app
+
+@app.errorhandler(404)
+def four_Ow_four(error):
+    '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourOwfour.html'),404
